7d82913c5b9fc487249b30a2c109f4a3192af49b
IlyaSv1 <ilya.sv7490@gmail.com>
2025-11-25 12:09:33 +0500

    Обновлен проект для Prometheus, Grafana

--- App_practical/14_Calculator/Program.cs
+++ App_practical/14_Calculator/Program.cs
@@ -1,15 +1,45 @@
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddOpenTelemetry()
+.WithMetrics(meterProviderBuilder =>
+{
+    meterProviderBuilder.AddPrometheusExporter();
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+    "Microsoft.AspNetCore.Server.Kestrel");
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+    meterProviderBuilder.AddView("http.server.request.duration",
+    new ExplicitBucketHistogramConfiguration
+    {
+        Boundaries =
+        [
+            0,
+            0.005,
+            0.01,
+            0.025,
+            0.05,
+            0.075,
+            0.1,
+            0.25,
+            0.5,
+            0.75,
+            1,
+            2.5,
+            5,
+            7.5,
+            10
+        ]
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,5 +53,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Calculator}/{action=Index}/{id?}");
+
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
