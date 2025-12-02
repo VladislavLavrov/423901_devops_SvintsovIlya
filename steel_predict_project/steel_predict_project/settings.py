e73b5ec8b76305bdff023bd73843402df6ca04d5
IlyaSv1 <ilya.sv7490@gmail.com>
2025-12-02 12:24:32 +0500

    Добавлен Django-проект steel_predictor_app

--- steel_predict_project/steel_predict_project/settings.py
+++ steel_predict_project/steel_predict_project/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'steel_predict_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steel_db',
+        'USER': 'steel_user',
+        'PASSWORD': 'steel_pass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
